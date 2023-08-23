c9273d1ed7ddb4dd474df908f927444b786d5a5e
Maxwell Cofie <maxwell@hubtel.com>
2023-08-23 12:34:11 +0000

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    app: {
+        baseURL: '/web-frontend/', // baseURL: '/<repository>/'
+        buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+    },
+    devtools: {enabled: true},
+
 })
