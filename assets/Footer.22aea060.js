8fad2f6e20dc37ab234c468427b4038ba067b3a1
Maxwell Cofie <maxwell@hubtel.com>
2023-08-24 02:14:53 +0000

    Updates

--- assets/Footer.22aea060.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./entry.64a599e2.js";const t={name:"Footer"};function e(r,n,s,c,_,a){return null}const f=o(t,[["render",e]]);export{f as default};
---
+++ assets/Footer.c1aa2727.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./entry.e9598ccd.js";const t={name:"Footer"};function e(r,n,s,c,_,a){return null}const f=o(t,[["render",e]]);export{f as default};
