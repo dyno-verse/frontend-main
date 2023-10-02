26606160ac5c87ebd21a0b8760760d75efa784cb
Maxwell Cofie <maxwell@hubtel.com>
2023-10-02 11:46:09 +0000

    added new chnages

--- pages/book-a-demo.vue
+++ pages/book-a-demo.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="w-full">
     <SectionWrapper>
-      <div class="flex justify-center flex-row align-middle">
-        <div class="md:w-1/2">
-          <div class="flex flex-col text-center">
+      <div class="flex justify-center flex-row min-h-screen max-h-fit">
+        <div class="md:w-1/2 align-middle">
+          <div class="flex flex-col text-center align-middle mt-60">
             <LeadTitle label="Book a demo"></LeadTitle>
             <p class="text-3xl">You're obe step closer in turning</p>
             <p class="text-3xl">hunger to happiness</p>
 
             <div class="flex flex-col w-full items-center">
-              <input type="text" class="border w-1/2 rounded-lg p-2 my-2">
-              <input type="text" class="border w-1/2 rounded-lg p-2 my-2">
-              <input type="text" class="border w-1/2 rounded-lg p-2 my-2">
-              <input type="text" class="border w-1/2 rounded-lg p-2 my-2">
+              <form class="w-full">
+                <div>
+                  <input id="name" type="text" class="border border-neutral-100 focus:border-red-600 w-1/2 rounded-lg p-4 my-2">
+                  <label for="name" class="float-left">Your Name</label>
+                </div>
+              </form>
             </div>
 
-            <Button :label="'Request a demo'" :type="ButtonTypes.Primary" class="w-full"/>
+            <Button :label="'Request a demo'" :type="ButtonTypes.Primary" class="w-120"/>
           </div>
         </div>
         <div class="md:w-1/2 bg-red-800 max-h-fit min-h-screen">
@@ -33,5 +35,30 @@ import {ButtonTypes} from "~/components/Constants";
 </script>
 
 <style scoped>
+form > div {
+  position: relative;
+}
+
+form > div > label {
+  position: absolute;
+}
+
+form label {
+  position: absolute;
+  top: 25px;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  transition: 0.2s;
+}
+
+form input:focus + label {
+  /*top: 100%;*/
+  margin-top: -16px;
+  margin-left: -10px;
+  transform: scale(0.7);
+
+}
+
 
 </style>
