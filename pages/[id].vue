f6e3ce82b1206cfeb28fa70ad5d065c3fc95001b
Maxwell Cofie <maxwell@hubtel.com>
2024-03-16 09:38:22 +0000

    added empty state

--- pages/[id].vue
+++ pages/[id].vue
@@ -3,8 +3,7 @@
 
 
     <div v-if="hasError" class="h-screen">
-      <p class="text-4xl font-black text-gray-900 dark:text-white align-middle text-center py-40">Page not found</p>
-
+      <EmptyState/>
     </div>
     <div v-else>
       <Loader v-if="isPending" class="py-80"/>
@@ -37,7 +36,8 @@
             <div
                 class="py-5 bg-white z-50 px-5 overflow-x-scroll flex overflow-x-auto space-x-8">
               <span v-for="category in categories" class="flex-shrink-0">
-                <a :href="`#${toKebabCase(category.name)}`">{{ category.name }}</a>
+                <a v-if="category.items.length !== 0" :href="`#${toKebabCase(category.name)}`"
+                   class="p-3">{{ category.name }}</a>
               </span>
             </div>
           </div>
@@ -48,8 +48,9 @@
 
           <!--    Categories-->
           <div v-for="category in categories" class="">
-            <h1 :id="toKebabCase(category.name)" v-if="category.items.length !== 0"
-                class="mx-5 mt-4 mb-2 text-xl font-medium">{{ category.name }}</h1>
+            <span :id="toKebabCase(category.name)" v-if="category.items.length !== 0" class="mt-20">
+              <h1 class="mx-5 mt-4 mb-2 text-xl font-medium">{{ category.name }}</h1>
+            </span>
             <CardStripAction v-for="item in category.items" :title="item.name" :amount="item.price"
                              :img-url="item.imageUrl"/>
           </div>
@@ -69,6 +70,7 @@ import {IBusinessInfo} from "~/repository/models/ApiResponse";
 import {useRoute} from "vue-router";
 import CardStripAction from "~/components/units/CardStripAction.vue";
 import Loader from "~/components/units/Loader.vue";
+import EmptyState from "~/components/units/EmptyState.vue";
 
 const {$api} = useNuxtApp();
 const businessInfo = ref({} as IBusinessInfo)
