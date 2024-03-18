97cbc7de48f0bc5f95fe35aa086065e8235b2908
Maxwell Cofie <maxwell@hubtel.com>
2024-03-18 17:42:07 +0000

    pushed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,39 @@
 <template>
-  <div class="">
-    <div class="">
-
+  <div class="container mx-auto">
+    <div class="w-full flex flex-row justify-between py-5">
+      <div class="flex flex-row items-center space-x-6">
+        <NuxtLink to="/" active-class="text-red-500 font-medium">
+          <img src="assets/imgs/logo.svg" class="h-12 w-12">
+        </NuxtLink>
+        <NuxtLink to="solutions" class="font-medium text-sm">
+          What's dyno menu?
+        </NuxtLink>
+        <NuxtLink to="solutions" class="font-medium text-sm">
+          Solutions
+        </NuxtLink>
+        <NuxtLink to="pricing" active-class="text-red-500 font-medium" class="text-sm">
+          Pricing
+        </NuxtLink>
+        <NuxtLink to="blog" class="text-sm font-medium">
+          Blog
+        </NuxtLink>
+      </div>
+      <!--Buttons-->
+      <div class="flex flex-row space-x-2">
+        <button type="button"
+                class="text-red-500 bg-red-50 font-medium rounded-lg font-medium text-sm px-5 py-2.5 mb-2">
+          Log In
+        </button>
+        <button type="button"
+                class="text-white bg-red-500 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">
+          Book a demo
+        </button>
+      </div>
     </div>
-    <slot/>
-    <LazyBaseFooter></LazyBaseFooter>
+    <slot class="w-full"/>
+    <div class="w-full">
+      <LazyBaseFooter></LazyBaseFooter>
+    </div>
   </div>
 </template>
 
