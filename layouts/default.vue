4a0035eb3434d94faf31583763df13cff3cd8fd9
Maxwell Cofie <maxwell@hubtel.com>
2024-03-19 08:12:08 +0000

    fixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <div class="w-full flex flex-row justify-between w-5/6 mx-auto py-5">
+    <div class="flex flex-row items-center justify-between mx-auto py-5 w-5/6">
       <div class="flex flex-row items-center space-x-6">
         <NuxtLink to="/" active-class="text-red-500 font-medium">
           <img src="assets/imgs/logo.svg" class="h-12 w-12">
@@ -21,11 +21,11 @@
       <!--Buttons-->
       <div class="flex flex-row space-x-2">
         <button type="button"
-                class="text-red-500 bg-red-50 font-medium rounded-lg font-medium text-sm px-5 py-2.5 mb-2">
+                class="text-red-500 bg-red-50 font-medium rounded-lg font-medium text-sm px-5 py-2.5">
           Log In
         </button>
         <button type="button"
-                class="text-white bg-red-500 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">
+                class="text-white bg-red-500 font-medium rounded-lg text-sm px-5 py-2.5">
           Book a demo
         </button>
       </div>
